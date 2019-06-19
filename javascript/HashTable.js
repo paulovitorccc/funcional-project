e9813f3fdeec1021946047fa08880bd89d4b4932
dalesEwerton <dales.fragoso@ccc.ufcg.edu.br>
2019-06-18 23:21:33 -0300

    Using the functional paradigm in JS code

--- javascript/HashTable.js
+++ javascript/HashTable.js
@@ -1,93 +1,133 @@
-// hashtable (inicio)
 class HashTable {
-    constructor(size) {
-      this.elements = 0;
-      this.size = size;
-      this.buckets = [];
-      for (let i = 0; i < size; i++) {
-        this.buckets.push(new this.elements([]));
-      }
-    }
-    empty() {
-      return this.elements === 0;
-    }
-    clear() {
-      this.buckets.forEach(bucket => {
-        bucket.clear();
-      });
-      this.elements = 0;
-    }
-    begin(item) {
-        var previous = undefined; 
-        if(this.hashFunction(item)){
-            this.buckets[item] = item;
-         }
-        else {
-            this.elements++;         
-         }
-         this.buckets[item] = item;
-        return previous;
-    }
-    insert(item) {
-      if (this.find(item)) return;
-      let pos = this.hashFunction(item);
-      this.buckets[pos].push_back(item);
-      this.elements++;
-    }
-    erase(item) {
-      const pos = this.hashFunction(item);
-      const list = this.buckets[pos];
-      const itemToErase = list.find(item);
-      if (itemToErase !== list.end()) {
-        list.erase(itemToErase);
-        this.elements--;
-      }
-    }
-    find(item) {
-      let pos = this.hashFunction(item);
-      return this.buckets[pos].find(item);
-    }
-    hashFunction(item) {
-        return this.buckets.hasOwnProperty(item);
-    }
-  }
   
-// hashtable (fim)
+	constructor(size=10){
+		this.table = new Array(size)
+		this.collisions = new Array(size)
+		this.size = size;
+	}
 
-// implementando a function alert (inicio)
-function alert(x){ 
-    x === 'undefined' ? console.log('undefined') : console.log(x); return; 
- }; 
- alert('x'); alert();
-// implementando a function alert (fim)
+	hash(value) {
+		return (typeof value) === "number" ? (value % this.size)
+										   : (value.toString().length % this.size);	
+	}
 
-// teste (inicio)
-var h = new HashTable({one: 1, two: 2, three: 3, "i'm no 4": 4});
+	isEmpty() {
+		return !this.table.some(
+					(element) => {
+						return (element ? true : false);
+					}
+				);
+	}
 
-console.log(h);
-h.empty();
-h.begin("anthony"); 
-h.begin
-console.log(h.elements);
-console.log("O hashtble", h);
-console.log("o bucket", h.buckets);
-console.log("o tamanho", h.elements);
+	capacity() {
+		return this.size;
+	}
 
-h.begin("bianca"); 
-h.begin
-console.log(h.elements);
-console.log("O hashtble", h);
-console.log("o bucket", h.buckets);
-console.log("o tamanho", h.elements);
+	tableSize() {
+		return this.table.filter((element) => {return (element !== undefined)}).length;
+	}
 
-h.begin("anthony"); 
-h.begin
-console.log(h.elements);
-console.log("O hashtble", h);
-console.log("o bucket", h.buckets);
-console.log("o tamanho", h.elements);
+	indexOf(value) {
+		return this._indexOfAux(value, 0)
+	}
 
-console.log(h.buckets);
-console.log(h);
+	insert(value) {
+		let position = this.hash(value);
+		return this._insertAux(position, value);
+	}
 
-// teste (fim)
+	remove(value) {
+		const index = this.indexOf(value);
+		if (index === undefined)
+			return 'Element not found';
+		this.table[index] = 'removed';
+	}
+
+	search(value) {
+		const index = this.indexOf(value);
+		return (index === undefined ? undefined : this.table[index]);
+	}
+
+	toConsole() {
+		console.table(this.table);
+	}
+
+	_insertAux(position, value) {
+
+		if (this.table[position] === undefined || this.table[position] === 'removed') {
+			// No collision case
+			this.table[position] = value;
+			this.collisions[position] = 0;
+
+			this._tryGrowUp();
+
+			return position; 
+		} else {
+			// Collision Case
+			const canInsert = this._checkInsertion(position, value);
+
+			if(canInsert) {
+				this.collisions[position] = this.collisions[position] + 1;
+				return this._insertAux(position+1, value)
+			}
+
+			return 'Duplicated elements not allowed'
+		}
+
+	}
+
+	_checkInsertion(position, value) {
+		return (this.table[position] === value ? false : true);
+	}
+
+	_indexOfAux(value, atualIndex) {
+		if(atualIndex >= this.size) return undefined;
+		return this.table[atualIndex] === value ? atualIndex : this._indexOfAux(value, atualIndex + 1);
+	}
+
+	_tryGrowUp() {
+		if (this.tableSize() > (this.size * 0.5)) {
+			console.log('Table size duplicated');
+			this.size = this.size * 2;
+			this.table[this.size] = undefined;
+			this.collisions[this.size] = undefined;
+		}
+	}
+
+}
+
+
+// Daqui pra baixo é pra interface, amanha vou separar
+const ht = new HashTable();
+
+// Inserir uns pra começar
+ht.insert(2)
+ht.insert(3)
+ht.insert(4)
+ht.insert(6)
+
+function update_table() {
+	$('#here_table').empty();
+	$('#here_table').append( '<thead><td>' + 'Index'  + '</td><td>Value</td></thead>' );
+	 for(i=0;i<ht.size;i++){
+	 	let value = (ht.table[i] === undefined ? 'Ø' : ht.table[i])
+	    $('#here_table').append( '<tbody><td>' +  i + '</td><td>' + value +'</td></tbody>' );
+	  }
+	$('#here_table').append(  '</table>' );
+}
+
+function insert() {
+	const value = $('#insert').val();
+	ht.insert(value);
+	$('#insert').val('');
+	update_table();
+}
+
+function remove() {
+	const value = $('#remove').val();
+	ht.remove(value);
+	$('#remove').val('');
+	update_table();
+}
+
+update_table();
